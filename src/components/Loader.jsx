eb14129ad8381841e2ecbd39b2af74b0e6202c7b
daniyalk0 <getdaniyalkhan@gmail.com>
2025-01-14 11:14:38 +0530

    f

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,39 +2,35 @@ import React, { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 import loaderAnimation from "../assets/loader.json";
 
-const Loader = ({ children }) => {
-  const [opacity, setOpacity] = useState(true);
-
+const Loader = ({children}) => {
+  const [opacity, setOpacity] = useState(true)
   useEffect(() => {
-    // Remove the initial loader (in public/index.html)
-    const initialLoader = document.getElementById("initial-loader");
-    if (initialLoader) {
-      initialLoader.remove();
-    }
-
-    // Set a timeout for the loader
-    const timer = setTimeout(() => {
-      setOpacity(false);
+    setTimeout(() => {
+      setOpacity(false)
     }, 1990);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  }, [])
+  useEffect(() => {
+    // Toggle scrolling based on `isUnlocked`
+    document.body.style.overflow = !opacity ? "auto" : "hidden";
+    
+    // Cleanup on unmount
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [opacity]);
+  
   return (
     <>
-      <div
-        className={`fixed top-0 left-0 z-[2000] w-full h-full flex transition-opacity duration-300 items-center justify-center bg-white ${
-          opacity ? "opacity-1" : "opacity-0 pointer-events-none"
-        }`}
-      >
-        <Lottie
-          animationData={loaderAnimation}
-          loop={false}
-          style={{ height: "300px", width: "300px" }}
-        />
-      </div>
-      {!opacity && children}
-    </>
+    <div className={`fixed top-0 left-0 z-[2000] w-full h-full flex transition-all duration-300 items-center justify-center bg-white ${opacity ? 'opacity-1' : 'opacity-0'}`}>
+       <Lottie
+        animationData={loaderAnimation}
+        loop={false} // Disable loop if the loader ends
+        style={{ height: "300px", width: "300px" }}
+        /> 
+
+    </div>
+    {children}
+        </>
   );
 };
 
